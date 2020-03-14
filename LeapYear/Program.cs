8e708f8c7d36060d22ef23aa957f875d6c7d5095
magnetoplasmadynamics <link1922@gmail.com>
2020-03-14 15:53:57 -0500

    message

---
+++ LeapYear/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int i = 2020; i <= 2097; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
